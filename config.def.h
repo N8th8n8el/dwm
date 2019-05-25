92c7bbfc12289ec4561a494a61c10516382a5a68
Nathanael <nathanael97@protonmail.com>
2019-05-25 20:26:46 +0200

    dito

--- config.def.h
+++ config.def.h
@@ -12,8 +12,8 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-#define baralpha        = 0xd0;
-#define borderalpha     = OPAQUE;
+#define baralpha        0xd0
+#define borderalpha     OPAQUE
 static const unsigned int *alphas[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { borderalpha, baralpha, OPAQUE },
